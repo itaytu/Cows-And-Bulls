1fb8ba0dc07ac5804d55c5e7eddab71e4f1e9f25
itaytu <itaytu93@gmail.com>
2019-04-17 16:02:29 +0300

    first version

--- Demo.cpp
+++ Demo.cpp
@@ -23,12 +23,10 @@ int main() {
 	cout << play(c1234, g12345, 4, 100) << endl;  // prints 101 - guesser loses technically by making an illegal guess (too long).
 	cout << play(c12345, g1234, 4, 100) << endl;  // prints 0 - chooser loses technically by choosing an illegal number (too long).
 
-	//RandomChooser randy;
-	// RandomGuesser guessy;
-	ConstantChooser randy("122657");
-	ConstantGuesser guessy("123127");
+	RandomChooser randy;
+	RandomGuesser guessy;
 	for (uint i=0; i<100; ++i) {
-		cout << play(randy, guessy, 6, 100) << endl;  // guesser should often win but sometimes lose.
+		cout << play(randy, guessy, 2, 100) << endl;  // guesser should often win but sometimes lose.
 	}
 
 	SmartGuesser smarty;
