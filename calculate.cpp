ceb268eef9871b9c729af3260df012a5cdadd2eb
moshe <moshelh95@gmail.com>
2019-04-28 18:59:54 +0300

    "commit"

--- calculate.cpp
+++ calculate.cpp
@@ -2,27 +2,28 @@
 #include "iostream"
 
 using namespace std;
+namespace bullpgia {
+    string calculateBullAndPgia(string choice, string guess) {
+        string result;
+        int bull = 0;
+        int pgia = 0;
+        string newChoice = choice;
+        string newGuess = guess;
 
-string bullpgia::calculateBullAndPgia(string choice, string guess) {
-    string result;
-    int bull = 0;
-    int pgia = 0;
-    string newChoice = choice;
-    string newGuess = guess;
-
-    for (int i = 0; i < choice.length(); ++i) {
-        if(choice[i] == guess[i]) {
+        for (int i = 0; i < choice.length(); ++i) {
+            if (choice[i] == guess[i]) {
                 bull++;
                 int index = newChoice.find(choice[i]);
                 newChoice.erase(index, 1);
                 newGuess.erase(index, 1);
+            }
         }
-    }
 
-    for (int i = 0; i < newChoice.length(); ++i) {
-        if(newChoice.find(newGuess[i]) != string::npos) pgia++;
-    }
+        for (int i = 0; i < newChoice.length(); ++i) {
+            if (newChoice.find(newGuess[i]) != string::npos) pgia++;
+        }
 
-    result = to_string(bull) + ',' + to_string(pgia);
-    return result;
+        result = to_string(bull) + ',' + to_string(pgia);
+        return result;
+    }
 }
