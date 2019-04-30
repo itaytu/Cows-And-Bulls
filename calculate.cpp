a3368fb7f06b5a87cba2b4dad791edd116cf57ec
moshe <moshelh95@gmail.com>
2019-04-30 13:05:41 +0300

    COMMIT

--- calculate.cpp
+++ calculate.cpp
@@ -46,33 +46,3 @@ namespace bullpgia {
         return count;
     }
 }
-/*    string calculateBullAndPgia(string choice, string guess) {
-        string result;
-        int bull = 0;
-        int pgia = 0;
-        string newChoice = choice;
-        string newGuess = guess;
-
-        for (int i = 0; i < choice.length(); ++i) {
-            if (choice[i] == guess[i]) {
-                bull++;
-//                int index = newChoice.find(choice[i]);
-                int index = i;
-                newChoice.erase(index, 1);
-                newGuess.erase(index, 1);
-                i--;
-            }
-        }
-
-        string tmpChoise = newChoice;
-        for (int i = 0; i < newChoice.length(); ++i) {
-            if (tmpChoise.find(newGuess[i]) != string::npos) {
-                pgia++;
-                int index = tmpChoise.find(newGuess[i]);
-                tmpChoise.erase(index, 1);
-            }
-        }
-
-        result = to_string(bull) + ',' + to_string(pgia);
-        return result;
-    }*/
